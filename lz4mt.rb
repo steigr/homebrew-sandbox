3f00657952ee501f58ea901522471b97ebfa93d9
Mathias Kaufmann <me@stei.gr>
2014-02-04 17:44:52 +0100

    simple formula to build and install lz4mt

---
+++ lz4mt.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Lz4mt < Formula
+  homepage "https://github.com/t-mat/lz4mt"
+  head 'git://github.com/t-mat/lz4mt.git', :using => :git
+
+  # depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "make", "CC=clang"
+    bin.install 'lz4mt'
+  end
+end
